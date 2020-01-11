01706a18d4d7b03eac2995a1702eb43363200cd8
Evgeniy Kirillov <kirillov@siberian.pro>
2020-01-11 16:30:23 +0700

    Альтернативное решение

--- 3-6-3.py
+++ 3-6-3.py
@@ -1,30 +1,30 @@
+from selenium import webdriver
+import pytest
 import time
 import math
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
+final = ''
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def browser():
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
-    yield browser
-    browser.quit()
+    br = webdriver.Chrome()
+    yield br
+    br.quit()
+    print(final)  # напечатать ответ про Сов в конце всей сессии
 
-@pytest.mark.parametrize('number', ["236895","236896","236897","236898","236899","236903","236904","236905"])
-def test_guest_should_see_login_link(browser, number):
-    link = f"https://stepik.org/lesson/{number}/step/1"
+
+@pytest.mark.parametrize('lesson', ['236895', '236896', '236897', '236898', '236899', '236903', '236904', '236905'])
+def test_find_hidden_text(browser, lesson):
+    global final
+    link = f'https://stepik.org/lesson/{lesson}/step/1'
+    browser.implicitly_wait(10)
     browser.get(link)
-    area =  browser.find_element_by_css_selector(".textarea")
     answer = math.log(int(time.time()))
-    area.send_keys(str(answer))
-    print(answer)
-    btn = browser.find_element_by_css_selector(".submit-submission")
-    btn.click()
-    msg = browser.find_element_by_css_selector(".smart-hints__hint").text
-    print(msg)
-    assert msg == "Correct!"
- 
+    browser.find_element_by_css_selector('textarea').send_keys(str(answer))
+    browser.find_element_by_css_selector('.submit-submission ').click()
+    check_text = browser.find_element_by_css_selector('.smart-hints__hint').text
+    try:
+        assert 'Correct!' == check_text
+    except AssertionError:
+        final += check_text  # собираем ответ про Сов с каждой ошибкой
